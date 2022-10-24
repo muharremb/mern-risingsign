7a7d8fa157023677a83b9d1ce1b5cf92474363b4
dan-lay <mr.danlay@gmail.com>
2022-10-24 13:12:12 -0400

    experiment with circular field component

---
+++ frontend/src/components/SignupPage/CircularField/CircularField.js
@@ -0,0 +1,11 @@
+import './CircularField.css';
+
+const CircularField = () => {
+   return (
+      <div className='circular-field'>
+         
+      </div>
+   )
+}
+
+export default CircularField;
