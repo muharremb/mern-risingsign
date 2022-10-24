2675786b90d3675f212eafead5616bc8d8709cf0
muharrem <boztepe.muharrem@gmail.com>
2022-10-24 09:49:02 -0400

    NavBar is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
+import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import MainPage from './components/MainPage/MainPage';
+import NavBar from './components/NavBar/NavBar';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+    <NavBar />
+    <h1>Hello from App Component</h1>
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+    </Switch>
+    </>
   );
 }
 
---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>Rising Sign MainPage Component</p>
+        <footer>
+          Copyright &copy; 2022
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
