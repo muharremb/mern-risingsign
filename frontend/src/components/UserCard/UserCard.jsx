ab7f1c8fcdf4c399ef1a0d2a340f697d08565754
cjsmi <ClarenceSmith90@gmail.com>
2022-10-26 13:30:12 -0400

    complete UserCard, users must have profileImage attached

--- frontend/src/components/UserCard/UserCard.jsx
+++ frontend/src/components/UserCard/UserCard.jsx
@@ -2,9 +2,16 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCurrentUser, logout } from '../../store/session';
 import { useEffect } from 'react';
+import { fetchUser } from '../../store/users';
 
 
 function UserCard({id}){
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUser(id));
+    }, [dispatch]);
 
     const user = useSelector(state => state.users[id])
     
@@ -14,14 +21,15 @@ function UserCard({id}){
     return (
         <div style={{"border":"1px solid red", "border-radius":"8px", "padding": "8px"}}>
             <h1>{user.name}</h1><br/>
+            <img src={user.profileImageURL} alt="Profile Image"/>
             <div className="user-card-sign sun-sign">
-                Sun-Sign Here
+                {user.horoscope.sun.label}: {user.horoscope.sun.Sign.label}
             </div>
-            <div className="user-card-sign sun-sign">
-                Moon-Sign Here
+            <div className="user-card-sign moon-sign">
+                {user.horoscope.moon.label}: {user.horoscope.moon.Sign.label}
             </div>
-            <div className="user-card-sign sun-sign">
-                Rising-Sign Here
+            <div className="user-card-sign rising-sign">
+                {user.horoscope.rising.label}: {user.horoscope.rising.Sign.label}
             </div>
         </div>
     )
