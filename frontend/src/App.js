2693b537c37e24bb7da33d18d04ee61a8350eeee
dan-lay <mr.danlay@gmail.com>
2022-10-28 08:26:09 -0400

    last minute changes before school

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,16 +35,16 @@ function App() {
 
   return loaded && (
     <>
-    <NavBar />
-    <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginForm} />
-      <AuthRoute exact path="/signup" component={SignupForm} />
+      <NavBar />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
 
-      <ProtectedRoute exact path="/profile" component={Profile} />
-      <ProtectedRoute exact path="/discover" component={Discover} />
-      <ProtectedRoute exact path="/chats" component={Chat} />
-    </Switch>
+        <ProtectedRoute exact path="/profile" component={Profile} />
+        <ProtectedRoute exact path="/discover" component={Discover} />
+        <ProtectedRoute exact path="/chats" component={Chat} />
+      </Switch>
     </>
   );
 }
