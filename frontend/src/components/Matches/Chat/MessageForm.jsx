3dfda0e2dc1d6743d57512d7660dbf5605fbc750
Benjamin Schwartz <bs4308@nyu.edu>
2022-11-14 17:50:28 -0500

    Last push before merge

--- frontend/src/components/Matches/Chat/MessageForm.jsx
+++ frontend/src/components/Matches/Chat/MessageForm.jsx
@@ -10,9 +10,16 @@ function MessageForm () {
   const { rememberRoom, setRememberRoom, socket, messages, setMessages, currentRoom, setCurrentRoom, storeRoom, currentRoomName } = useContext(ChatContext);
 
   socket.off('room-messages').on('room-messages', (roomMessages) => {
-    // console.log(formatMessages(roomMessages))
     setMessages(roomMessages)
   })
+
+  const [typing, setTyping] = useState(false)
+
+  //format bubbles
+  useEffect(()=> {
+    socket.on('bubbles', () => setTyping(true))
+    socket.on('stop-bubbles', ()=> setTyping(false) )
+  }, [])
 
   const formatMinutes = (minutes) => {
     if (minutes < 10) return '0' + String(minutes)
@@ -25,43 +32,41 @@ function MessageForm () {
     const date = (dateObj.getMonth() + 1) + '/' + dateObj.getDate() +  '/' + (dateObj.getYear() - 100);
     const time = dateObj.getHours() + ":" + formatMinutes(dateObj.getMinutes())
     const hackyRoomName = localStorage.getItem("currentRoom")
-    // console.log( `current room is called ${hackyRoomName}`)
     socket.emit('message-room', hackyRoomName, msg, user, time, date);
     setMsg("")
     const messagesEl = document.getElementById("message-box")
     messagesEl.scrollTop = messagesEl.scrollHeight; // might need null protection?
-
+    socket.emit('stop-bubbles', hackyRoomName)
+    setTyping(false)
   }
-
-  // useEffect(()=> {
-  //   messagesList = messages?.messagesByDate ? Object.values(messages.messagesByDate) : null;
-  //   messagesList = messagesList?.map((message, i) => <li key={i} id={message._id} className={`chat-message ${message.from._id === user._id ? "you" : "them"}`}>{message.content}</li>)
-  //   // messagesList1 = formatMessages(messagesList1)
-  // }, [msg])
 
   const formatMessages = (messages) => {
-    const reactMessages = []
-    // console.log("might return early")
     if (!messages) return
-    console.log(messages)
+    const reactMessages = []
     for (let i=0; i<messages.length; i++) {
       reactMessages.push(<h3>{messages[i]._id}</h3>)
-      console.log(messages[i]._id)
-      // console.log(messages[i].messagesByDate)
       for (let j=0; j<messages[i].messagesByDate.length; j++){
-        console.log(messages[i].messagesByDate[j].content)
-        reactMessages.push(<li>{messages[i].messagesByDate[j].content}</li>)
+        reactMessages.push(<li className={`chat-message ${messages[i].messagesByDate[j].from._id === user._id ? "you" : "them"}`}><span>{formatTime(messages[i].messagesByDate[j].time)} &nbsp;</span>{messages[i].messagesByDate[j].content}</li>)
       }
     }
-
     return reactMessages.reverse()
-
   }
 
+  const formatTime = (time) => {
+    const [hours, minutes] = time.split(":")
+    return hours < 12 ? time + " am" : hours-12 + ":" + minutes + " pm"
+  }
 
-  let messagesList = messages?.messagesByDate ? Object.values(messages.messagesByDate) : null;
-
-  messagesList = messagesList?.map((message, i) => <li key={i} id={message._id} className={`chat-message ${message.from._id === user._id ? "you" : "them"}`}>{message.content}</li>)
+  const handleChange = (e) => {
+    setMsg(e.target.value)
+    if (!typing) {
+      setTyping(true)
+      socket.emit('bubbles', localStorage.getItem("currentRoom") )
+      setTimeout(() => {
+        setTyping(false)
+        socket.emit('stop-bubbles',localStorage.getItem("currentRoom") )
+      }, 1000)
+  }}
 
   return (
     <>
@@ -70,6 +75,7 @@ function MessageForm () {
 
         <div className="display-messages" id="message-box">
           {/* {messagesList} */}
+          {typing && '...'}
           {formatMessages(messages)}
         </div>
 
@@ -78,7 +84,23 @@ function MessageForm () {
         <input type="text"
         placeholder="be nice"
         value={msg}
-        onChange={(e) => setMsg(e.target.value)}
+        onChange={(e) => {
+          setMsg(e.target.value)
+          console.log('message is', msg)
+          console.log(e.target.value==="")
+          setTyping(true)
+          socket.emit('bubbles', localStorage.getItem("currentRoom") )
+          if (e.target.value==="") {
+            console.log('empty message box')
+            socket.emit('stop-bubbles', localStorage.getItem("currentRoom") )
+            setTyping(false)
+          }
+      //     setTimeout(() => {
+      //       socket.emit('stop-bubles', localStorage.getItem("currentRoom"))
+      //     setTyping(false)
+      // }, 1000)
+
+        } }
         />
         <button type="submit">
         <i className="fa-solid fa-plane-departure"/></button>
