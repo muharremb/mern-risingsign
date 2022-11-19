8164f5d38d523c11b1681d4e12706c7a15ab70fb
Benjamin Schwartz <bs4308@nyu.edu>
2022-11-19 15:45:24 -0500

    Add click out of modal functionality

--- frontend/src/components/MatchedModal/MatchedModal.jsx
+++ frontend/src/components/MatchedModal/MatchedModal.jsx
@@ -15,6 +15,12 @@ function MatchedModal(props){
         document.getElementById('matched-modal').close();
         // props.modalFunc(false)
     }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        document.getElementById('matched-modal').close();
+
+        }
+      }
 
     useEffect(() => {
         console.log("modal component rendering first time")
@@ -28,7 +34,7 @@ function MatchedModal(props){
           <img className="modal-pic" src={profUrl}></img>
                 <h1>You matched with {props.name}!</h1>
                 <Link to="/matches">Go to Matches Page to Chat</Link>
-                <button onClick={closeModal}>Stay Here</button>
+                {/* <button onClick={closeModal}>Stay Here</button> */}
             </div>
         </dialog>
     )
