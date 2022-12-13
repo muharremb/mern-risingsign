6e4de8ece7792217bc9efdcf0a9c254dabe24023
Benjamin Schwartz <bs4308@nyu.edu>
2022-12-13 12:53:53 -0500

    Change socket endpt

--- frontend/src/context/chatContext.js
+++ frontend/src/context/chatContext.js
@@ -1,7 +1,12 @@
 import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
-const ENDPT = "http://localhost:5000" //is this correct/?
+let ENDPT = "http://localhost:5000" //is this correct/?
+if (process.env.NODE_ENV === 'production') {
+  let ENDPT = 'https://rising-sign.onrender.com/'
+  console.log("in production mode")
+
+}
 
 export const socket = io(ENDPT)
 
